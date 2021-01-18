9e9f846a5c045c1a9192c51636ef3218bdb67946
Milos Kallai <52494559+mls-kll@users.noreply.github.com>
2021-01-18 16:31:59 +0100

    Delete next.config.js

--- pig-shop/next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  env: {
-    contentfulSpace: '4op1z3eu1szs',
-    contentfulAccessToken: 'aGsBh6Y-P6ObRjLtgh6CZwlglPq0sPzan_WBXUtbdVY',
-  },
-  publicRuntimeConfig: {
-    baseUrl: 'http://localhost:3000',
-  },
-};
