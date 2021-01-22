8ec5939c0cab6a7b48d8d58221480b7fc2fa7145
Milos.Kallai <milos.kallai@guidesmiths.com>
2021-01-22 11:49:26 +0100

    add dependencies to useCart hook

--- pig-shop/src/hooks/cart.js
+++ pig-shop/src/hooks/cart.js
@@ -4,14 +4,20 @@ import { useCartContext } from '../context/cartContext';
 export const useCart = () => {
   const { cartState, setCartState } = useCartContext();
 
-  const handleAddToCart = useCallback((id) =>
-    setCartState((oldState) => [...oldState, { id }])
+  const handleAddToCart = useCallback(
+    (id) => {
+      setCartState((oldState) => [...oldState, { id }]);
+    },
+    [cartState]
   );
 
-  const handleDeleteFromCart = useCallback((selectedId) => {
-    const newState = cartState.filter((item) => item.id !== selectedId);
-    setCartState(newState);
-  });
+  const handleDeleteFromCart = useCallback(
+    (selectedId) => {
+      const newState = cartState.filter((item) => item.id !== selectedId);
+      setCartState(newState);
+    },
+    [cartState]
+  );
 
   return [handleAddToCart, handleDeleteFromCart];
 };
